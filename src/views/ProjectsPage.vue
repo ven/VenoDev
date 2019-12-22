a8ae04951a8e034becadbb247fa393cb940afeff
Ven <electioxvenomancy@gmail.com>
2019-12-22 18:48:50 +0000

    Fix unresponsiveness on mobile on projects view.

--- src/views/ProjectsPage.vue
+++ src/views/ProjectsPage.vue
@@ -1,26 +1,28 @@
 <template>
     <div class="ProjectsPage">
         <div class="hero bg-purple-700 pt-24 pb-24">
-            <div class="container mx-auto">
+            <div class="container mx-auto p-6">
                 <div class="text-white text-5xl font-bold"><i class="fas fa-leaf mr-3"></i>Projects</div>
                 <div class="text-gray-300 text-2xl font-medium">A collection of active projects.</div>
 
                 <hr class="my-8">
                 
-                <section class="text-white">
-                    <div class="flex flex-wrap -mb-4">
-                        <div class="info container w-1/3 bg-purple-800 p-16 rounded-lg shadow-lg mb-8 mr-4">
-                            <div class="font-bold text-2xl"><i class="fab fa-discord mr-3"></i>Centro</div>
-                        </div>
-
-                        <div class="info container w-1/3 bg-purple-800 p-16 rounded-lg shadow-lg mb-8 mr-4">
-                            <div class="font-bold text-2xl"><i class="fab fa-discord mr-3"></i>Apex Tracker</div>
-                        </div>
-
-                        <div class="info container w-1/3 bg-purple-800 p-16 rounded-lg shadow-lg mb-8 mr-4">
-                            <div class="font-bold text-2xl"><i class="fas fa-globe-europe mr-3"></i>Risky World</div>
+                <section class="text-white py-16">
+                    <div class="px-2">
+                        <div class="flex flex-wrap -mx-2 text-center">
+                            <div class="sm:w-1/2 w-full px-2">
+                               <div class="info container bg-purple-800 p-16 rounded-lg shadow-lg mb-8">
+                                    <div class="font-bold text-2xl"><i class="fab fa-discord mr-3"></i>Centro</div>
+                                </div>
+                            </div>
+                            <div class="sm:w-1/2 w-full px-2">
+                                <div class="info container bg-purple-800 p-16 rounded-lg shadow-lg mb-8">
+                                    <div class="font-bold text-2xl"><i class="fab fa-discord mr-3"></i>Apex Tracker</div>
+                                </div>
+                            </div>
                         </div>
                     </div>
+                    
                 </section>
             </div>
         </div>
