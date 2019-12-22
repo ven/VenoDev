cdbad80e4314da702da537f321066c13f280a911
Ven <electioxvenomancy@gmail.com>
2019-12-22 18:54:51 +0000

    Add mobile navbar responsiveness.

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -19,13 +19,13 @@
                 <span class="font-semibold text-xl tracking-tight">veno.dev</span>
             </div>
             
-            <div class="block lg:hidden">
+            <div @click="toggle" class="block lg:hidden">
                 <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
                 <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
                 </button>
             </div>
 
-            <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+            <div :class="open ? 'block': 'hidden'" class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
                 <div class="text-sm lg:flex-grow text-white">
                     <router-link to="/">
                         <a class="block mt-4 lg:inline-block lg:mt-0 hover:text-indigo-300 mr-4">
@@ -52,6 +52,22 @@
         </nav>
     </div>
 </template>
+
+<script>
+export default {
+    name: 'Navbar',
+    data: function() {
+        return {
+            open: false,
+        }
+    },
+    methods: {
+        toggle() {
+            this.open = !this.open
+        }
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 .add-spin {
